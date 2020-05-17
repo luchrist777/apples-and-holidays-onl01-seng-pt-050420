5826143a7cb867b00425142b1bdc1b3912b868f3
Luis Leandro <luchrist777@gmail.com>
2020-05-17 06:12:36 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
-all_supplies(holiday_supplies)
+require_relative '../lib/holiday'
 
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  holiday_hash.map do |season, holidays|
-    holidays.map do |holiday, supplies|
-      holiday if supplies.include?("BBQ")
-    end
-  end.flatten.compact
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  #config.order = 'default'
 end
-all_holidays_with_bbq(holiday_supplies)
